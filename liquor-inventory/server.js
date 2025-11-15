1ce4d350a1707878c3341ab1d776f1274990f559
ejunior6981 <ejunior6981@gmail.com>
2025-11-15 22:48:29 +0000

    Update server to run on port 3600

--- liquor-inventory/server.js
+++ liquor-inventory/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 3600;
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
